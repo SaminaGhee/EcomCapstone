52c3a0ab91783985c2427b4199b2329ed194e76f
SaminaGreen <samantha.green91@outlook.com>
2024-04-19 13:11:40 -0500

    generated project

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Capstone E-com</h1>
+      </div>
+    );
+  }
+}
