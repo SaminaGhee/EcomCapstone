edae5f55d4ee941a5cd143dcb08ec593de0b6a8a
SaminaGreen <samantha.green91@outlook.com>
2024-04-25 13:15:01 -0500

    created project, add image carousel and images

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,44 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import '../style/app.css'; // CSS file
+import Carousel from './carousel.js'; // Carousel component
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className='app'>
-        <h1>Capstone E-com</h1>
-      </div>
-    );
+const App = () => {
+  const [cart, setCart] = useState([]);
+
+  const images = [
+    { src: 'images/classic fringe.png', alt: 'Product 1', price: 55 },
+    { src: 'images/boho fringe.png', alt: 'Product 2', price: 50 },
+    { src: 'images/bobbles.png', alt: 'Product 3', price: 45 }
+  ];
+
+  const addToCart = (product) => {
+    setCart([...cart, product]);
   }
-}
+
+  return (
+    <div className="container">
+      <h1>PillowLove</h1>
+      <Carousel images={images} addToCart={addToCart} />
+      <h2>Products</h2>
+      <div className="product-container">
+        {images.map((product, index) => (
+          <div key={index} className='product'>
+            <img src={product.src} alt={product.alt} />
+            <p>{product.alt} - ${product.price}</p>
+            <button onClick={() => addToCart(product)}> Add to Cart</button>
+            </div>
+        ))}
+      </div>
+      <h2>Shopping Cart</h2>
+      <div>
+        {cart.map((item, index) => (
+          <div key={index}>
+            <p>{item.alt} - ${item.price}</p>
+            </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/carousel.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+const Carousel = ({ images }) => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
+
+  return (
+    <div className="carousel-container">
+      <Slider {...settings}>
+        {images.map((image, index) => (
+          <div key={index}>
+            <img src={product.src} alt={product.alt} />
+            <p>{product.alt} - ${product.price}</p>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+};
+
+export default Carousel;
